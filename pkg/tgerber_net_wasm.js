a01957124d81671136322ddffcb200ff4d54fa68
Fl1tzi <kontakt@tgerber.net>
2022-09-24 23:36:00 +0200

    wasm fixing

---
+++ pkg/tgerber_net_wasm.js
@@ -0,0 +1,2 @@
+import * as wasm from "./tgerber_net_wasm_bg.wasm";
+export * from "./tgerber_net_wasm_bg.js";
---
+++ pkg/tgerber_net_wasm_bg.js
@@ -0,0 +1,25 @@
+import * as wasm from './tgerber_net_wasm_bg.wasm';
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachedUint8Memory0 = new Uint8Array();
+
+function getUint8Memory0() {
+    if (cachedUint8Memory0.byteLength === 0) {
+        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+
+export function __wbindgen_throw(arg0, arg1) {
+    throw new Error(getStringFromWasm0(arg0, arg1));
+};
+
